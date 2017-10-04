6d50c1ba70848a8731f15fcd381064223101e5a0
Anthematics <jasonharder2@gmail.com>
2017-10-04 17:20:31 -0400

    console logs message on send request

--- ajax.js
+++ ajax.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-$.ajax({
-	url:'/',
-	method: 'GET',
-	data: ,
-	datatype: 'TEXT',
+		let button = document.querySelector("#step12");
 
-})
+		button.addEventListener("click",function(event){
 
-});
+				$.ajax({
+					url:'http://first-ajax-api.herokuapp.com/',
+					method: 'GET'
 
+					//data: ,
+					//datatype: 'TEXT',
+
+		}).done(function(){
+			console.log('I am Andys boyfriend now.');
+		});
+	});
 });
