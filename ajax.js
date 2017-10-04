64256a81361aee926bd7100037445d1e2975ae5a
Anthematics <jasonharder2@gmail.com>
2017-10-04 18:44:25 -0400

    v 0.2 Rewrote code to take multiple URLS and any selector passed in as an arguement

--- ajax.js
+++ ajax.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-		let button = document.querySelector("#step12");
+	function buttonListen(url,selector) {
 
-		button.addEventListener("click",function(event){
+	let buttons = document.querySelector(selector);
+	 buttons.addEventListener("click",function(event){
+			$.ajax({
+				url: url,
+				method: 'GET',
 
-				$.ajax({
-					url:'http://first-ajax-api.herokuapp.com/',
-					method: 'GET'
+				//data: ,
+				//datatype: 'TEXT',
 
-					//data: ,
-					//datatype: 'TEXT',
-
-		}).done(function(){
-			console.log('I am Andys boyfriend now.');
+			}).done(function(response){
+				console.log('done',response);
+			}).fail(function () {
+				let fail = createElement('p'); //lets us create the message as p tag <p>
+					fail.innerHTML = "you fail" //sets the message as you fail
+					selector.appendChild(p); // appends the message to the selector which was defined as an arguement in button listen allowing it to work everywhere.
+			});
 		});
-	});
+	}
+
+buttonListen("http://first-ajax-api.herokuapp.com",'#step12')
 });
